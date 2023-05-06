9b484f814a2a2f1bfd561ca6d5483a63d43a4503
EdgarVRP <edgarvrp22@gmail.com>
2023-05-06 14:12:23 -0600

    Se actualizan los endpoints de los servicios web desplegados

--- src/views/public/js/script.js
+++ src/views/public/js/script.js
@@ -1,5 +1,12 @@
 //URL_Backend: http://localhost:3001/adminPrestatario/crear
+/* LOCAL
 let URL_Backend_prestatario= `http://localhost:3001/`;
 let URL_Backend_evaluacion= `http://127.0.0.1:5000/`;
 let URL_Backend_proyecto= `http://localhost:8080/`;
 let URL_Backend_files= `http://localhost:3002/`;
+
+*/
+let URL_Backend_prestatario= `https://softni-syncprestatario-production.up.railway.app/`;
+let URL_Backend_evaluacion= `https://softni-syncevaluacion-production.up.railway.app/`;
+let URL_Backend_proyecto=`https://softni-syncproyecto-production.up.railway.app/`;
+let URL_Backend_files= `https://softni-syncfiles-production.up.railway.app/`;
