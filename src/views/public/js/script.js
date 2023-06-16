b66786df5e671c46626e94fdf7d6c11fad3543d1
EdgarVRP <edgarvrp22@gmail.com>
2023-06-15 22:55:30 -0600

    Se añaden URL

--- src/views/public/js/script.js
+++ src/views/public/js/script.js
@@ -10,3 +10,10 @@ let URL_Backend_prestatario= `https://softni-syncprestatario-production-ddf6.up.
 let URL_Backend_evaluacion= `https://softni-syncevaluacion-production-6699.up.railway.app/`;
 let URL_Backend_proyecto=`https://softni-syncproyecto-production-f120.up.railway.app/`;
 let URL_Backend_files= `https://softni-syncfiles-production-4d6b.up.railway.app/`;
+
+/*
+let URL_Backend_prestatario= `http://172.16.10.215:3001/`;
+let URL_Backend_evaluacion= `http://172.16.10.215:8000/`;
+let URL_Backend_proyecto=`http://172.16.10.215:8081/`;
+let URL_Backend_files= `http://172.16.10.215:3002/`;
+*/
